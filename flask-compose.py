457916145e1bb904c29751f8b3e231ef2f80d5e2
Sebastian Cheung CQF <sebastian_cheung@yahoo.com>
2018-03-03 15:58:34 +0100

    added text

--- flask-compose.py
+++ flask-compose.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Docker within PyCharm!'
+    return 'Hello from Docker on Saturday within PyCharm!'
 
 
 if __name__ == '__main__':
